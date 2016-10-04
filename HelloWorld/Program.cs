3cf5ed70ffc0f08c5b317be47f819a64b0d420cf
SarunasV <SarunasV@KMKPC7531>
2016-10-04 10:58:27 +0300

    add no arguments handling

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -6,8 +6,15 @@ namespace HelloWorld
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello world!");
+            if (args.Length < 1)
+            {
+                Console.WriteLine("Application requires at least 1 argument!");
+                return;
+            }
 
+            string name = args[0];
+
+            Console.WriteLine(string.Format("Hello, {0}!", name));
             Console.ReadLine();
         }
     }
