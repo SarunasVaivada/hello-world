5c4b9cf8918202d965097905817ceacf6623010a
SarunasV <SarunasV@KMKPC7531>
2016-10-18 09:41:51 +0300

    short linq sort implementation

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace HelloWorld
 {
@@ -6,15 +7,28 @@ namespace HelloWorld
     {
         private static void Main(string[] args)
         {
-            if (args.Length < 1)
+
+            var input = @"SAS 9.3 - CI Studio, SDM Studio and Enterprise Guide is working fine and Metadata refresh job completed successfully overnight.";
+            var counts = input.GroupBy(c => c).Select(g => new { Char = g.Key, Count = g.Count() });
+
+            //counts.OrderByDescending(g => g.Count);
+
+            foreach (var item in counts.OrderByDescending(g => g.Count))
             {
-                Console.WriteLine("Application requires at least 1 argument!");
-                return;
+                Console.WriteLine($"Character: {item.Char} is repeated: {item.Count} times");
             }
 
-            var module = new HelloModule();
 
-            Console.WriteLine(module.SayHello(args[0]));
+
+            //if (args.Length < 1)
+            //{
+            //    Console.WriteLine("Application requires at least 1 argument!");
+            //    return;
+            //}
+
+            //var module = new HelloModule();
+
+            //Console.WriteLine(module.SayHello(args[0]));
             Console.ReadLine();
         }
     }
